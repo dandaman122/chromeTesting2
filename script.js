55e787b33d69bd5500478b14670b836a43fd1586
dandaman122 <144803866+dandaman122@users.noreply.github.com>
2024-03-29 17:03:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function die() {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", '_blank'); }
+
+    console.log('hello');
+
+ for (let i = 0; i < 4; i++) {
+    die();
+  }  
+
+   /* while(1 == 1) {
+    die();
+  } */
